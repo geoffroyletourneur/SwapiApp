19e4360b3e3491d9bc19d3a0965ba2768d2474dc
Geoffroy LETOURNEUR <geoffroy.letourneur@leroymerlin.fr>
2023-04-11 14:42:46 +0200

    :tada: Create SwapiApp

---
+++ SwapiApp/SwapiApp/SwapiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwapiAppApp.swift
+//  SwapiApp
+//
+//  Created by Geoffroy LETOURNEUR on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwapiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwapiApp/SwapiAppUITests/SwapiAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwapiAppUITestsLaunchTests.swift
+//  SwapiAppUITests
+//
+//  Created by Geoffroy LETOURNEUR on 11/04/2023.
+//
+
+import XCTest
+
+final class SwapiAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
